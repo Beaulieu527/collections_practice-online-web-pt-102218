a0c68aa560da86ead2dc60f7ee5105297f8e6d35
Justin Beaulieu <beaulieudev@gmail.com>
2018-10-31 01:13:35 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -4,15 +4,19 @@ def sort_array_asc(array)
 end
 
 def sort_array_desc(array)
+  array.sort!
 end
 
 def sort_array_char_count(array)
+  array.sort{|a, b| a.length <=> b.length}
 end
 
 def swap_elements(array)
+  array.swap
 end
 
 def reverse_array(array)
+  array.reverse
 end
 
 def kesha_maker(array)
