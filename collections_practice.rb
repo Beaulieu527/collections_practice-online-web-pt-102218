62061f2a663f38b0a8a19fca56012e907f67be75
Justin Beaulieu <beaulieudev@gmail.com>
2018-10-31 02:29:48 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -36,10 +36,11 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect do |word, i |
+  array.each_with_index.collect do |word, i|
     if i == 1
       word
     else
       word + "s"
-    end 
+    end
+  end
 end
